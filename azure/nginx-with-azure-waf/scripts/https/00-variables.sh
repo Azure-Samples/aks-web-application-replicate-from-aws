c10c59528a2c213275ac572f3bed0be967a3b867
Paolo Salvatori <paolos@microsoft.com>
2024-10-21 11:27:15 +0200

    Additional sanitization

--- azure/nginx-with-azure-waf/scripts/https/00-variables.sh
+++ azure/nginx-with-azure-waf/scripts/https/00-variables.sh
@@ -1,11 +1,11 @@
 # Azure Resources
-RESOURCE_GROUP_NAME="AnubisRG"
+RESOURCE_GROUP_NAME="<your-resource-group-name>"
 SUBSCRIPTION_ID=$(az account show --query id --output tsv)
 SUBSCRIPTION_NAME=$(az account show --query name --output tsv)
 TENANT_ID=$(az account show --query tenantId --output tsv)
-AKS_CLUSTER_NAME="AnubisAks"
-AGW_NAME="AnubisApplicationGateway"
-AGW_PUBLIC_IP_NAME="AnubisApplicationGatewayPublicIp"
+AKS_CLUSTER_NAME="<your-aks-group-name>"
+AGW_NAME="<your-application-gateway-name>"
+AGW_PUBLIC_IP_NAME="<your-application-gateway-public-ip-name>"
 DNS_ZONE_NAME="<your-azure-dns-name-eg-contoso-com>"
 DNS_ZONE_RESOURCE_GROUP_NAME="<your-azure-dns-resource-group-name>"
 DNS_ZONE_SUBSCRIPTION_ID='<your-azure-dns-subscription-id>'
@@ -20,7 +20,7 @@ NGINX_REPLICA_COUNT=3
 
 # Ingress and DNS
 INGRESS_CLASS_NAME="nginx"
-SUBDOMAIN="anubisyelb"
+SUBDOMAIN="<your-yelb-application-subdomain>"
 URL="https://$SUBDOMAIN.$DNS_ZONE_NAME"
 
 # Secret Provider Class
